6f8c727524187152f47aaaebbe1f863084fb1876
brxnw <burhoniddinoff@gmail.com>
2024-02-22 00:58:33 +0500

    splash

--- app/src/main/java/com/sudo_pacman/asaxiybooks/app/MyApp.kt
+++ app/src/main/java/com/sudo_pacman/asaxiybooks/app/MyApp.kt
@@ -1,4 +1,7 @@
 package com.sudo_pacman.asaxiybooks.app
 
-class MyApp {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application()
