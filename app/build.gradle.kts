949f3fa55781d3b6432098dadba22e817b79afc1
Muhammad <assalomalaykum14@gmail.com>
2024-02-22 08:08:31 +0500

    basic library added to gradle and create screen_info.xml, screen_read.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,13 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("androidx.navigation.safeargs.kotlin")
+
+    id("com.google.gms.google-services")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -41,4 +48,38 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // pdf viewer
+    implementation("com.github.barteksc:android-pdf-viewer:2.8.2")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    /**
+     * Navigation
+     * */
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    /**
+     *   viewBinding
+     */
+    implementation("com.github.kirich1409:viewbindingpropertydelegate-noreflection:1.5.9")
+
+    /**
+     *  Dagger hilt
+     */
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-compiler:2.50")
+
+    /**
+     *  Firebase
+     */
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
+    // Add the dependency for the Cloud Storage library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-storage")
+
 }
