c83d1e91dcc4ea0cffdff8634190238e11864d8f
Muhammad <assalomalaykum14@gmail.com>
2024-02-22 15:02:13 +0500

    change nav data

--- app/src/main/java/com/sudo_pacman/asaxiybooks/app/MyApp.kt
+++ app/src/main/java/com/sudo_pacman/asaxiybooks/app/MyApp.kt
@@ -1,8 +1,6 @@
 package com.sudo_pacman.asaxiybooks.app
 
 import android.app.Application
-import com.sudo_pacman.asaxiybooks.domain.impl.RepositoryAddBook
-import com.sudo_pacman.asaxiybooks.domain.impl.TestViewModel
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
--- app/src/main/java/com/sudo_pacman/asaxiybooks/presenter/screen/info/InfoScreen.kt
+++ app/src/main/java/com/sudo_pacman/asaxiybooks/presenter/screen/info/InfoScreen.kt
@@ -3,14 +3,22 @@ package com.sudo_pacman.asaxiybooks.presenter.screen.info
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.sudo_pacman.asaxiybooks.R
 import com.sudo_pacman.asaxiybooks.databinding.ScreenInfoBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class InfoScreen : Fragment(R.layout.screen_info) {
     private val binding by viewBinding(ScreenInfoBinding::bind)
+    private val navArgs by navArgs<InfoScreenArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val bookData = navArgs.book
+
+
     }
 }
