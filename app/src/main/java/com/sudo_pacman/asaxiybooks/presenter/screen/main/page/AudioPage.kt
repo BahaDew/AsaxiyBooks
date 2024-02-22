845414f9a0b2246dbd80dfd93d08598ddf0bae86
boxodiryusubjonov7418 <boxodiryusubjonov7418@gmail.com>
2024-02-22 15:07:53 +0500

    CategoryByBookData

--- app/src/main/java/com/sudo_pacman/asaxiybooks/presenter/screen/main/page/AudioPage.kt
+++ app/src/main/java/com/sudo_pacman/asaxiybooks/presenter/screen/main/page/AudioPage.kt
@@ -1,7 +1,20 @@
 package com.sudo_pacman.asaxiybooks.presenter.screen.main.page
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.sudo_pacman.asaxiybooks.R
+import com.sudo_pacman.asaxiybooks.databinding.PageAudioBinding
 
 class AudioPage : Fragment(R.layout.page_audio) {
+    private val binding by viewBinding(PageAudioBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun initView() {
+
+    }
 }
