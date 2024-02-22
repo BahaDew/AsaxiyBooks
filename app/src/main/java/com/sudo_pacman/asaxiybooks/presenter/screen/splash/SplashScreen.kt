4e1c4feca7d78134042dc7cbaec646a95a3be7c8
brxnw <burhoniddinoff@gmail.com>
2024-02-22 14:15:10 +0500

    register bag fix it

--- app/src/main/java/com/sudo_pacman/asaxiybooks/presenter/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/sudo_pacman/asaxiybooks/presenter/screen/splash/SplashScreen.kt
@@ -1,7 +1,6 @@
 package com.sudo_pacman.asaxiybooks.presenter.screen.splash
 
 import android.annotation.SuppressLint
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
