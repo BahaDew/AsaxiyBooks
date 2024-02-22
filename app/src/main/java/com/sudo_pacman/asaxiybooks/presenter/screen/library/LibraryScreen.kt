26354d84e391e1843a0b293f82a2134da470a633
behruz <bekhacode@gmail.com>
2024-02-22 11:51:37 +0500

    class

---
+++ app/src/main/java/com/sudo_pacman/asaxiybooks/presenter/screen/library/LibraryScreen.kt
@@ -0,0 +1,8 @@
+package com.sudo_pacman.asaxiybooks.presenter.screen.library
+
+import androidx.fragment.app.Fragment
+import com.sudo_pacman.asaxiybooks.R
+
+class LibraryScreen : Fragment(R.layout.screen_library){
+
+}
