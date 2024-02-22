fc33da82651222bf16e149501457e8c63f1bf522
brxnw <burhoniddinoff@gmail.com>
2024-02-22 16:36:34 +0500

    audio

--- app/src/main/java/com/sudo_pacman/asaxiybooks/domain/impl/RepositoryImpl.kt
+++ app/src/main/java/com/sudo_pacman/asaxiybooks/domain/impl/RepositoryImpl.kt
@@ -15,7 +15,7 @@ class RepositoryImpl {
     private val fireStore = Firebase.firestore
 
     val booksList: MutableSharedFlow<List<BookUIData>> = MutableSharedFlow(replay = 1, onBufferOverflow = BufferOverflow.DROP_LATEST)
-    val bookLoadError: MutableSharedFlow<String> =  MutableSharedFlow(replay = 1, onBufferOverflow = BufferOverflow.DROP_LATEST)
+    val bookLoadError: MutableSharedFlow<String> = MutableSharedFlow(replay = 1, onBufferOverflow = BufferOverflow.DROP_LATEST)
 
     fun getBooks() {
         fireStore
@@ -23,24 +23,23 @@ class RepositoryImpl {
             .addSnapshotListener { value, error ->
                 val books = mutableListOf<BookUIData>()
 
-                value?.forEach {snapshot ->
+                value?.forEach { snapshot ->
 
-                    if (snapshot.data.getOrDefault("bookUrl", "").toString().isNotEmpty()) {
-                        books.add(
-                            BookUIData(
-                                docID = snapshot.id,
-                                author = snapshot.data.getOrDefault("author", "").toString(),
-                                bookUrl = snapshot.data.getOrDefault("bookUrl", "").toString(),
-                                categoryId = snapshot.data.getOrDefault("categoryId", "").toString(),
-                                coverImage = snapshot.data.getOrDefault("coverImage", "").toString(),
-                                description = snapshot.data.getOrDefault("description", "").toString(),
-                                filePath = snapshot.data.getOrDefault("filePath", "").toString(),
-                                name = snapshot.data.getOrDefault("name", "").toString(),
-                                totalSize = snapshot.data.getOrDefault("totalSize", "").toString(),
-                                type = snapshot.data.getOrDefault("pdf", "").toString(),
-                            )
+                    books.add(
+                        BookUIData(
+                            docID = snapshot.id,
+                            audioUrl = snapshot.data.getOrDefault("audioUrl", "").toString(),
+                            author = snapshot.data.getOrDefault("author", "").toString(),
+                            bookUrl = snapshot.data.getOrDefault("bookUrl", "").toString(),
+                            categoryId = snapshot.data.getOrDefault("categoryId", "").toString(),
+                            coverImage = snapshot.data.getOrDefault("coverImage", "").toString(),
+                            description = snapshot.data.getOrDefault("description", "").toString(),
+                            filePath = snapshot.data.getOrDefault("filePath", "").toString(),
+                            name = snapshot.data.getOrDefault("name", "").toString(),
+                            totalSize = snapshot.data.getOrDefault("totalSize", "").toString(),
+                            type = snapshot.data.getOrDefault("pdf", "").toString(),
                         )
-                    }
+                    )
                 }
 
                 if (error != null) bookLoadError.tryEmit(error.message.toString())
--- app/src/main/java/com/sudo_pacman/asaxiybooks/presenter/screen/info/InfoScreen.kt
+++ app/src/main/java/com/sudo_pacman/asaxiybooks/presenter/screen/info/InfoScreen.kt
@@ -3,22 +3,27 @@ package com.sudo_pacman.asaxiybooks.presenter.screen.info
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.sudo_pacman.asaxiybooks.R
 import com.sudo_pacman.asaxiybooks.databinding.ScreenInfoBinding
+import com.sudo_pacman.asaxiybooks.presenter.screen.intro.IntroScreenDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class InfoScreen : Fragment(R.layout.screen_info) {
     private val binding by viewBinding(ScreenInfoBinding::bind)
-    private val navArgs by navArgs<InfoScreenArgs>()
+//    private val navArgs by navArgs<InfoScreenArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val bookData = navArgs.book
+//        val bookData = navArgs.book
 
+        binding.btnListen.setOnClickListener {
+            findNavController().navigate(InfoScreenDirections.actionInfoScreenToAudioScreen())
+        }
 
     }
 }
