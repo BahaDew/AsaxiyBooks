bde0aeb58e5f342a41fb2c5103afd66af1062a97
brxnw <burhoniddinoff@gmail.com>
2024-02-23 15:17:18 +0500

    sign in

--- app/src/main/java/com/sudo_pacman/asaxiybooks/presenter/viewModel/LoginVM.kt
+++ app/src/main/java/com/sudo_pacman/asaxiybooks/presenter/viewModel/LoginVM.kt
@@ -3,7 +3,7 @@ package com.sudo_pacman.asaxiybooks.presenter.viewModel
 import kotlinx.coroutines.flow.Flow
 
 interface LoginVM {
-    val successLoginFlow:Flow<Unit>
-    val errorMessage:Flow<String>
-    fun loginUser(password:String,gmail:String)
+    val successLoginFlow: Flow<Unit>
+    val errorMessage: Flow<String>
+    fun loginUser(password: String, gmail: String)
 }
